284e6017903845458a64b723b36dd7cc94006610
sallybananas <saljgiani@gmail.com>
2018-02-24 11:55:17 -0700

    idk

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -41,3 +41,12 @@ $(document).ready(function(){
 	// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 	$('#add-modal').modal('open');
 });
+
+$('.datepicker').pickadate({
+	selectMonths: true, // Creates a dropdown to control month
+	selectYears: 99, // Creates a dropdown of 15 years to control year,
+	today: 'Today',
+	clear: 'Clear',
+	close: 'Ok',
+	closeOnSelect: false // Close upon selecting a date,
+});

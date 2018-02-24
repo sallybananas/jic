58fdb3832f48601318ff2d828bd86174050360ea
Kristoffer Keene <toffer2001@yahoo.com>
2018-02-24 11:56:46 -0700

    sign up

--- server.js
+++ server.js
@@ -2,6 +2,97 @@ var express = require("express");
 var upload = require('jquery-file-upload-middleware');
 var bodyParser = require("body-parser");
 var app = express();
+var multer = require('multer');
+var upload = multer(); 
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+//Express sessions
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+require("./routes/api-routes.js")(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
+app.use(cookieParser());
+app.use(session({ secret: "Your secret key" }));
+
+var Users = [];
+
+app.get('/signup', function (req, res) {
+    console.log("signed up");
+    res.render('signup');
+    
+});
+
+app.post('/signup', function (req, res) {
+    console.log("signed up");
+    if (!req.body.email || !req.body.password) {
+        res.status("400");
+        res.send("Invalid details!");
+    } else {
+        Users.filter(function (user) {
+            if (user.email === req.body.email) {
+                res.render('signup', {
+                    message: "User Already Exists! Login or choose another user email"
+                });
+            }
+        });
+        var newUser = { email: req.body.email, password: req.body.password };
+        Users.push(newUser);
+        req.session.user = newUser;
+        res.redirect('/main');
+    }
+});
+
+function checkSignIn(req, res) {
+    if (req.session.user) {
+        next();     //If session exists, proceed to page
+    } else {
+        var err = new Error("Not logged in!");
+        console.log(req.session.user);
+        next(err);  //Error, trying to access unauthorized page!
+    }
+}
+app.get('/main', checkSignIn, function (req, res) {
+    res.render('main', { email: req.session.user.email })
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.post('/login', function (req, res) {
+    console.log(Users);
+    if (!req.body.email || !req.body.password) {
+        res.render('login', { message: "Please enter both email and password" });
+    } else {
+        Users.filter(function (user) {
+            if (user.email === req.body.email && user.password === req.body.password) {
+                req.session.user = user;
+                res.redirect('/main');
+            }
+        });
+        res.render('login', { message: "Invalid credentials!" });
+    }
+});
+
+app.get('/logout', function (req, res) {
+    req.session.destroy(function () {
+        console.log("user logged out.")
+    });
+    res.redirect('/login');
+});
+
+app.use('/main', function (err, req, res, next) {
+    console.log(err);
+    //User should be authenticated! Redirect him to log in.
+    res.redirect('/login');
+});
+
+
 
 var PORT = process.env.PORT || 3000;
 
